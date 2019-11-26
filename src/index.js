7af3d41272717209f8004ebdedef8e4ab99fc13d
Kevin <kevinktom@gmail.com>
2019-11-26 15:32:50 -0500

    removed modal comments

--- src/index.js
+++ src/index.js
@@ -302,12 +302,12 @@ function myMove(e){
                     && (c !== boundX || r !== boundY) //possibly affect dragging quality of start
                     && startDrag === true
                     ) {
-                    // debugger
+
                     currentStart.val = 10;
                     graph.nodes[c][r].val = 1;
                     currentStart = graph.nodes[c][r];
                     graph.starting = currentStart;
-                    // debugger
+
                     boundX = c;
                     boundY = r;
                 }
@@ -316,12 +316,12 @@ function myMove(e){
                     && (c !== boundX || r !== boundY) //possibly affect dragging quality of end
                     && endDrag === true
                     ) {
-                    // debugger
+
                     currentEnd.val = 10;
                     graph.nodes[c][r].val = 2;
                     currentEnd = graph.nodes[c][r];
                     graph.ending = currentEnd;
-                    // debugger
+
                     boundX = c;
                     boundY = r;
                 }
@@ -345,31 +345,28 @@ function getMousePosition(canv, event){
 }
 
 var modal = document.getElementById("myModal");
-// debugger
-// Get the button that opens the modal
+
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+
 btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-
 
 });
 
